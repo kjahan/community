54281928f41592ccb671aa0de412456aa29a1080
Kazem Jahanbakhsh <jahan@Hercule-2.local>
2013-12-05 22:07:23 -0800

    Cleaned the code and add main.

--- cmty.py
+++ cmty.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 import networkx as nx
-import pkg_resources
-pkg_resources.require("matplotlib")
-import matplotlib.pylab as plt
 import math
 import csv
 import random as rand
+import sys
 
 #this method just reads the graph structure from the file
-def buildG( G, file_, delimiter_ ):
+def buildG(G, file_, delimiter_):
     #construct the weighted version of the contact graph from cgraph.dat file
     #reader = csv.reader(open("/home/kazem/Data/UCI/karate.txt"), delimiter=" ")
     reader = csv.reader(open(file_), delimiter=delimiter_)
@@ -18,7 +16,7 @@ def buildG( G, file_, delimiter_ ):
 
 #Keep removing edges from Graph until one of the connected components of Graph splits into two.
 #compute the edge betweenness
-def CmtyGirvanNewmanStep( G ):
+def CmtyGirvanNewmanStep(G):
     #print "call CmtyGirvanNewmanStep"
     init_ncomp = nx.number_connected_components(G)    #no of components
     ncomp = init_ncomp
@@ -35,11 +33,12 @@ def CmtyGirvanNewmanStep( G ):
             if float(v)/float(G[k[0]][k[1]]['weight']) == max_:
                 G.remove_edge(k[0],k[1])    #remove the central edge
         ncomp = nx.number_connected_components(G)    #recalculate the no of components
+
 #compute the modularity of current split
-def _GirvanNewmanGetModularity( G, deg_):
+def _GirvanNewmanGetModularity(G, deg_, m_):
     New_A = nx.adj_matrix(G)
     New_deg = {}
-    UpdateDeg(New_deg, New_A)
+    New_deg = UpdateDeg(New_A)
     #Let's compute the Q
     comps = nx.connected_components(G)    #list of components    
     print 'no of comp: %d' % len(comps)
@@ -55,48 +54,62 @@ def _GirvanNewmanGetModularity( G, deg_):
     #print "Modularity: %f" % Mod
     return Mod
 
-def UpdateDeg(deg_, A_):
-    for i in range(0,n):
+def UpdateDeg(A):
+    deg_dict = {}
+    n = len(A)
+    for i in range(n):
         deg = 0.0
-        for j in range(0,n):
-            deg += A_[i,j]
-        deg_[i] = deg
+        for j in range(n):
+            deg += A[i,j]
+        deg_dict[i] = deg
+    return deg_dict
 
-#let's create a graph and insert the edges
-G = nx.Graph()
-buildG(G, 'graph.txt', ',')
-
-n = G.number_of_nodes()    #|V|
-#adjacenct matrix
-A = nx.adj_matrix(G)
-
-m_ = 0.0    #the weighted version for number of edges
-for i in range(0,n):
-    for j in range(0,n):
-        m_ += A[i,j]
-m_ = m_/2.0
-print "m: %f" % m_
-
-#calculate the weighted degree for each node
-Orig_deg = {}
-UpdateDeg(Orig_deg, A)
-
-#let's find the best split of the graph
-BestQ = 0.0
-Q = 0.0
-while True:    
-    CmtyGirvanNewmanStep(G)
-    Q = _GirvanNewmanGetModularity(G, Orig_deg);
-    print "current modularity: %f" % Q
-    if Q > BestQ:
-        BestQ = Q
-        Bestcomps = nx.connected_components(G)    #Best Split
-        print "comps:"
+#run GirvanNewman algorithm and find the best community split by maximizing modularity measure
+def runGirvanNewman(G, Orig_deg, m_):
+    #let's find the best split of the graph
+    BestQ = 0.0
+    Q = 0.0
+    while True:    
+        CmtyGirvanNewmanStep(G)
+        Q = _GirvanNewmanGetModularity(G, Orig_deg, m_);
+        print "current modularity: %f" % Q
+        if Q > BestQ:
+            BestQ = Q
+            Bestcomps = nx.connected_components(G)    #Best Split
+            print "comps:"
+            print Bestcomps
+        if G.number_of_edges() == 0:
+            break
+    if BestQ > 0.0:
+        print "Best Q: %f" % BestQ
         print Bestcomps
-    if G.number_of_edges() == 0:
-        break
-if BestQ > 0.0:
-    print "Best Q: %f" % BestQ
-    print Bestcomps
-else:
-    print "Best Q: %f" % BestQ
+    else:
+        print "Best Q: %f" % BestQ
+
+def main(argv):
+    if len(argv) < 2:
+        sys.stderr.write("Usage: %s <input graph>\n" % (argv[0],))
+        return 1
+    graph_fn = argv[1]
+    G = nx.Graph()  #let's create the graph first
+    buildG(G, graph_fn, ',')
+
+    n = G.number_of_nodes()    #|V|
+    A = nx.adj_matrix(G)    #adjacenct matrix
+
+    m_ = 0.0    #the weighted version for number of edges
+    for i in range(0,n):
+        for j in range(0,n):
+            m_ += A[i,j]
+    m_ = m_/2.0
+    print "m: %f" % m_
+
+    #calculate the weighted degree for each node
+    Orig_deg = {}
+    Orig_deg = UpdateDeg(A)
+
+    #run Newman alg
+    runGirvanNewman(G, Orig_deg, m_)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
